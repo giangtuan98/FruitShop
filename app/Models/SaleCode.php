131415b0e1fc006a0b6db0c3cc65ea838b7c3086
giangtuan6199@gmail.com <giangtuan6199@gmail.com>
2020-12-04 21:52:30 +0700

    create models

---
+++ app/Models/SaleCode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SaleCode extends Model
+{
+    use HasFactory;
+
+    protected $table = "sale_codes";
+}
